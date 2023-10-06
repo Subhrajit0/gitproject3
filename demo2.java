b4dd3c576b3043a1c6da2359c90d03d22079c9cb
Subhrajit <subhrajitmaharana*@gmail.com>
2023-10-06 07:24:25 +0530

    features: add

---
+++ demo2.java
@@ -0,0 +1,11 @@
+public class demo2 {
+
+	public static void main(String args[]) {
+
+		System.out.println("Hello JavaTechnocrat.") ;
+		System.out.println("Hello Subhrajit.") ;
+		System.out.println("Bye.") ;
+
+	}
+
+}
